455170d0d807125135beb7a47ac37cdc175dbd2e
beretkag <beretkag@gmail.com>
2025-07-05 18:25:36 +0200

    alap felépítés

--- app.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('gomb').addEventListener("click", ()=>{
-    alert("csao");
-})
---
+++ script.js
@@ -0,0 +1,2 @@
+// Egyszerű scroll-to-top funkció vagy egyéb JS bővítmény ide jöhet majd.
+console.log("Arany Légió weboldal betöltve.");
